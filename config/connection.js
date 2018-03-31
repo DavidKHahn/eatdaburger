9e712ca529ba678b6824d0a8cba59b0423d2becf
David Hahn <34965292+DavidKHahn@users.noreply.github.com>
2018-03-30 22:05:28 -0700

    Update connection.js
    updated Heroku files

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+   port: 3306,
   host: "localhost",
   user: "root",
   password: "",
-  database: "burgers_db"
+  database: "burgers_db",
+
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -19,4 +25,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+//connection.connect();
 module.exports = connection;
