57b8ff297a9c3e255f5fc3036da47dd800825dfe
David Hahn <34965292+DavidKHahn@users.noreply.github.com>
2018-04-09 14:58:35 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection;
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-<<<<<<< HEAD
+
 } else {
     connection = mysql.createConnection({
       port: 3306,
@@ -15,18 +15,7 @@ if(process.env.JAWSDB_URL){
       database: "burgers_db"
   });
 }
-=======
-}else{
-  connection = mysql.createConnection({
-   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db",
 
-});
-};
->>>>>>> 9e712ca529ba678b6824d0a8cba59b0423d2becf
 
 // Make connection.
 connection.connect(function(err) {
